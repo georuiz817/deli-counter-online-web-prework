3f9ca99589db9f26d3aedb29ce8c81a5943deb64
George Ruiz <georuiz817@gmail.com>
2019-05-28 20:46:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
+katz_deli = []
 
+def line (array)
+   if array.count == 0
+      puts "The line is currently empty."
+   else
+      response = "The line is currently:"
+      array.each_with_index do |name, index|
+         response += " #{index+1}. #{name}" 
+   end
+   puts response
+ end
+ end
+ 
